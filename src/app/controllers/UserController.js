f944933f93c48185b75624a78e558cc826e42d0c
Thiago Marinho <tgmarinho@gmail.com>
2019-09-13 12:17:03 -0400

    GoBarber - Aula 11 -  Cadastro de usuários

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,14 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Thiago Marinho',
-    email: 'tgmarinho@gmail.com',
-    password_hash: '1232131',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
