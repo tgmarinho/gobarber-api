1246cca5b19e4a63ce15739e95a8400763cf298a
Thiago Marinho <tgmarinho@gmail.com>
2019-09-17 10:38:50 -0400

    GoBarber - Aula 26 - Listando agenda do prestador

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,13 +1,3 @@
-## Aula 26 - Listando agenda do prestador
-
-- Mostar no painel do prestador de serviço a listagem de sua agenda
-- Criar uma nova rota para agenda do provider (schedule)
-- Criar um novo controller: ScheduleController.js
-- Verificar se o usuário logado é um provider(prestador)
-- buscar agenda pela data e fazer um parseISO
-- buscar os agendamentos do provedor logado, que não esteja cancelado e que a data seja no ínicio do dia buscado até o final do dia.
-
-```
 import { startOfDay, endOfDay, parseISO } from 'date-fns';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
@@ -44,5 +34,3 @@ class ScheduleController {
 }
 
 export default new ScheduleController();
-```
-Fim: [https://github.com/tgmarinho/gobarber/tree/aula26](https://github.com/tgmarinho/gobarber/tree/aula26)
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -22,6 +23,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
