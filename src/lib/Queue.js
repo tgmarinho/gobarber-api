f95bb8d7c61c8255577e21edc3d975ee756849e9
Thiago Marinho <tgmarinho@gmail.com>
2019-09-18 10:22:31 -0400

    GoBarber - Aula 35 - Monitorando falhas na fila

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -29,8 +29,12 @@ class Queue {
   processQueue() {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
-      bee.process(handle);
+      bee.on('failed', this.handleFailure).process(handle);
     });
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
   }
 }
 
